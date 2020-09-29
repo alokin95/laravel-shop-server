785e899fabbfba90baadc583748a61e9c5c0f2be
nikola.mitrovic <nikola.mitrovic@soprex.com>
2020-09-29 15:52:25 +0200

    npm build

--- app/Http/Controllers/Api/V1/GameController.php
+++ app/Http/Controllers/Api/V1/GameController.php
@@ -1,11 +1,10 @@
 <?php
 
-namespace App\Http\Controllers\Api;
+namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
+class GameController extends Controller
 {
-
 }
